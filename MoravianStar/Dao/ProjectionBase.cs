8e5149796fc10fb0349295aebef7f54b8ba7576a
Lyuboslav Neshev <l_neshev@abv.bg>
2023-04-13 18:45:28 +0300

    Add class ProjectionBase

---
+++ MoravianStar/Dao/ProjectionBase.cs
@@ -0,0 +1,20 @@
+﻿using MoravianStar.Dao;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.Common.Core.Projections
+{
+    /// <summary>
+    /// The base class of a projection containing Id.
+    /// </summary>
+    /// <typeparam name="TId">The type of the Id.</typeparam>
+    public class ProjectionBase<TId> : IProjectionBase<TId>
+    {
+        [Key]
+        public TId Id { get; set; }
+
+        public virtual bool IsNew()
+        {
+            return Id == null || Id.Equals(default(TId));
+        }
+    }
+}
