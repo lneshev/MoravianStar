d2d635b544623067115ba490594372bf8071ac6e
Lyuboslav Neshev <l_neshev@abv.bg>
2023-11-13 16:58:51 +0200

    Extract method "GetHttpStatusCode" as an exception extension, so that it can be reused

---
+++ MoravianStar.WebAPI/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using MoravianStar.Exceptions;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Security;
+
+namespace MoravianStar.WebAPI.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static int GetHttpStatusCode(this Exception exception)
+        {
+            if (exception == null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+
+            int result;
+
+            switch (exception)
+            {
+                case InvalidModelStateException _:
+                case ValidationException _:
+                case BusinessException _:
+                    result = (int)HttpStatusCode.BadRequest;
+                    break;
+                case SecurityException _:
+                case SecurityTokenExpiredException _:
+                    result = (int)HttpStatusCode.Unauthorized;
+                    break;
+                case EntityNotFoundException _:
+                    result = (int)HttpStatusCode.NotFound;
+                    break;
+                case MethodAccessException _:
+                case NotImplementedException _:
+                    result = (int)HttpStatusCode.MethodNotAllowed;
+                    break;
+                case EntityNotUniqueException _:
+                    result = (int)HttpStatusCode.Conflict;
+                    break;
+                default:
+                    result = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
--- MoravianStar.WebAPI/Middlewares/ExceptionMiddleware.cs
+++ MoravianStar.WebAPI/Middlewares/ExceptionMiddleware.cs
@@ -2,22 +2,18 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using MoravianStar.Exceptions;
+using MoravianStar.WebAPI.Extensions;
 using MoravianStar.WebAPI.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System;
-using System.ComponentModel.DataAnnotations;
-using System.Net;
 using System.Net.Mime;
-using System.Security;
 using System.Threading.Tasks;
 
 namespace MoravianStar.WebAPI.Middlewares
 {
     /// <summary>
-    /// Custom exception middleware that cathes any exception, logs the exception, creates a generic error model,
+    /// Custom exception middleware that catches any exception, logs the exception, creates a generic error model,
     /// generates a correct http status code related to the exception, puts it into the error model and writes the error model to the response.
     /// </summary>
     public class ExceptionMiddleware
@@ -50,7 +46,7 @@ namespace MoravianStar.WebAPI.Middlewares
             }
         }
 
-        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
         {
             var jsonSettings = new JsonSerializerSettings()
             {
@@ -65,50 +61,15 @@ namespace MoravianStar.WebAPI.Middlewares
             var result = JsonConvert.SerializeObject(error, jsonSettings);
 
             context.Response.Clear();
-            context.Response.StatusCode = GetHttpStatusCode(exception);
+            context.Response.StatusCode = exception.GetHttpStatusCode();
             context.Response.ContentType = MediaTypeNames.Application.Json;
 
-            return context.Response.WriteAsync(result);
+            await context.Response.WriteAsync(result);
         }
 
         private void LogCriticalException(Exception ex)
         {
-            logger.LogCritical(ex, ex.InnerException != null
-                ? ex.InnerException.Message
-                : ex.Message);
-        }
-
-        private int GetHttpStatusCode(Exception exception)
-        {
-            int result;
-
-            switch (exception)
-            {
-                case InvalidModelStateException _:
-                case ValidationException _:
-                case BusinessException _:
-                    result = (int)HttpStatusCode.BadRequest;
-                    break;
-                case SecurityException _:
-                case SecurityTokenExpiredException _:
-                    result = (int)HttpStatusCode.Unauthorized;
-                    break;
-                case EntityNotFoundException _:
-                    result = (int)HttpStatusCode.NotFound;
-                    break;
-                case MethodAccessException _:
-                case NotImplementedException _:
-                    result = (int)HttpStatusCode.MethodNotAllowed;
-                    break;
-                case EntityNotUniqueException _:
-                    result = (int)HttpStatusCode.Conflict;
-                    break;
-                default:
-                    result = (int)HttpStatusCode.InternalServerError;
-                    break;
-            }
-
-            return result;
+            logger.LogCritical(ex, ex.InnerException != null ? ex.InnerException.Message : ex.Message);
         }
     }
 }
