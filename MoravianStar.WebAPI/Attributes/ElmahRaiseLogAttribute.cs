ae58953d8160070cabf8bc04d3fbe5ec44104457
Lyuboslav Neshev <l_neshev@abv.bg>
2023-11-18 22:41:52 +0200

    Remove RaiseOnError option from ElmahRaiseLogAttribute

--- MoravianStar.WebAPI/Attributes/ElmahRaiseLogAttribute.cs
+++ MoravianStar.WebAPI/Attributes/ElmahRaiseLogAttribute.cs
@@ -7,20 +7,18 @@ namespace MoravianStar.WebAPI.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class ElmahRaiseLogAttribute : ActionFilterAttribute
     {
-        public ElmahRaiseLogAttribute(string message = null, bool raiseOnError = false)
+        public ElmahRaiseLogAttribute(string message = null)
         {
             Message = message;
-            RaiseOnError = raiseOnError;
         }
 
         public string Message { get; }
-        public bool RaiseOnError { get; }
 
         public override void OnResultExecuted(ResultExecutedContext context)
         {
             base.OnResultExecuted(context);
 
-            if (context.Exception == null || RaiseOnError)
+            if (context.Exception == null)
             {
                 ElmahExtensions.RaiseLog(Message);
             }
